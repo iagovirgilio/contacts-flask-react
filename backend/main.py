33ec80022bd959650b6a3d9da0317e8d2f4fa62c
Iago Virgílio <iagovirgilio@gmail.com>
2024-04-13 22:13:40 -0300

    Ordernar contacts id desc

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from flask import request, jsonify
+from sqlalchemy import desc
 from config import app, db
 from models import Contact
 
 @app.route('/contacts', methods=['GET'])
 def get_contacts():
-    contacts = Contact.query.all()
+    contacts = Contact.query.order_by(desc(Contact.id)).all()
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
 
